f8b9fd43c5bb34e0113fd4f15d4de9a4ff2aedbe
kiizerd <ajsewell13@gmail.com>
2022-03-11 20:30:17 -0600

    Add xs Tailwind breakpoint.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {  
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '480px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
